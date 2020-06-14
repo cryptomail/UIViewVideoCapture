049d7622924d483a4b844e528b991be29a9d6d44
Joshua Teitelbaum <joshuat@gmail.com>
2020-06-14 11:29:24 -0700

    Initial Commit

---
+++ UIViewVideoCapture/UIViewVideoCapture.h
@@ -0,0 +1,19 @@
+//
+//  UIViewVideoCapture.h
+//  UIViewVideoCapture
+//
+//  Created by Joshua Teitelbaum on 6/14/20.
+//  Copyright © 2020 sparklesparklesparkle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UIViewVideoCapture.
+FOUNDATION_EXPORT double UIViewVideoCaptureVersionNumber;
+
+//! Project version string for UIViewVideoCapture.
+FOUNDATION_EXPORT const unsigned char UIViewVideoCaptureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIViewVideoCapture/PublicHeader.h>
+
+
